89e742e0951b8fd113955ccde63faf61241b244b
jimole775 <727346595@qq.com>
2019-10-26 14:51:35 +0800

    增加几个favicon方案；取消打包Map资源

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -63,14 +63,41 @@
     }
   }
 </script>
-<style>
+<style rel="stylesheet/less" lang="less" scoped>
 
-  @import './login.less';
+  /* @import './login.less'; */
   .ivu-spin-fix {
     background-color: hsla(0, 76%, 50%, 0) !important
   }
 
   .spin-icon-load {
     animation: ani-demo-spin 1s linear infinite;
+  }
+  .login{
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    &-con{
+        position: absolute;
+        top: 50%;
+        transform: translateY(-60%);
+        width: 300px;
+        &-header{
+            font-size: 16px;
+            font-weight: 300;
+            text-align: center;
+            padding: 30px 0;
+        }
+        .form-con{
+            padding: 10px 0 0;
+        }
+        .login-tip{
+            font-size: 10px;
+            text-align: center;
+            color: #c3c3c3;
+        }
+    }
   }
 </style>
