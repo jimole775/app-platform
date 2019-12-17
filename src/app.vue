411fd7cd48c8571a1a96c4a356ac36e745b2756a
rongchuanda <rongchuanda@chinasie.com>
2019-12-17 10:03:51 +0800

    【代码规范】--- 格式化代码，以 eslint 为标准

--- src/app.vue
+++ src/app.vue
@@ -1,40 +1,47 @@
 <template>
   <div id="app">
     <!--主要用于切换 login 和 展示平台-->
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  mounted(){
-    window.fillerWidth = 100
-    const prevLoad = document.querySelector("#prevLoad")
-    const loadingText = document.querySelector(".loadingText")
-    loadingText.innerText = 'completion'
-    setTimeout(function () {   // 给与1S的时间填充满                   
-        prevLoad.style.opacity = 0
-        setTimeout(function () {  // 给与时间fade-out 
-            prevLoad.remove()
-        }, 500)
-    }, 1000)
+  name: "App",
+  mounted() {
+    this.completeStartAnima()
   },
-  created(){}
-}
-
+  created() {},
+  methods: {
+    completeStartAnima() {
+      window.fillerWidth = 100
+      const prevLoad = document.querySelector("#prevLoad")
+      const loadingText = document.querySelector(".loadingText")
+      loadingText.innerText = "completion"
+      setTimeout(function() {
+        // 给与1S的时间填充满
+        prevLoad.style.opacity = 0
+        setTimeout(function() {
+          // 给与时间fade-out
+          prevLoad.remove()
+        }, 500)
+      }, 1000)
+    }
+  }
+};
 </script>
 
-
 <style>
-#app,html,body {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+#app,
+html,
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height:100%;
-  width:100%;  
+  height: 100%;
+  width: 100%;
   min-height: 240px;
 }
 </style>
