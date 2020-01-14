f956691b529d81d0eebd6cb67728f395c134d2ef
rongchuanda <rongchuanda@chinasie.com>
2020-01-14 17:19:34 +0800

    【结构调整】--- 拆除winscp脚本

--- winscp_bat/build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO ON
-SET winscpscript=aa.txt
-SET srcdir=E:\test\vuedemo\
-cd /d %srcdir% && rmdir /s /q .\node_modules
-git stash save "temp" && git pull && yarn && git stash pop && npm run build
-winscp /script=%winscpscript%
--- winscp_bat/新建文本文档.bat
+++
@@ -1,9 +0,0 @@
-SET SRC_DIR=E:\project\remoteController
-SET IGNOR_1=readme.md
-cd /d %SRC_DIR% && rmdir /s /q .\node_modules 
-mkdir tmp && copy %SRC_DIR%\%IGNOR_1% %SRC_DIR%\tmp\
-git pull -f
-copy %SRC_DIR%\tmp\%IGNOR_1% %SRC_DIR%\%IGNOR_1%
-yarn install && npm run build
-
-PAUSE
