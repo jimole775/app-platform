6566826b08a26b23d69a5c55b45a8aee09fc6b61
rongchuanda <rongchuanda@chinasie.com>
2020-01-14 16:36:33 +0800

    完成deploy.bat逻辑

--- winscp_bat/prod/deploy.bat
+++ winscp_bat/prod/deploy.bat
@@ -12,33 +12,43 @@ set ignor[3]=package.json
 set ignor_count=3
 
 cls
+rem 进入项目上级目录
+cd /d %program_dist%
+
+rem 创建临时目录
+mkdir _tmp
+
 rem 首先把需要修改的文件进行备份
-mkdir %program_dist%\_tmp
 for /l %%i in (1,1,%ignor_count%) do (
-copy %program_dist%\%program_name%\!ignor[%%i]! %program_dist%\_tmp\
+copy .\%program_name%\!ignor[%%i]! .\_tmp\
 )
 
 rem 删除项目资源
-rd /s /q %program_dist%\%program_name%\
+rd /s /q %program_name%
 
 rem 然后使用git clone拉取新代码
 git clone %src_git%
 
 rem 把备份的文件覆盖到项目
 for /l %%j in (1,1,%ignor_count%) do (
-copy %program_dist%\_tmp\!ignor[%%j]! %cur_dist%\%program_name%\
+copy .\_tmp\!ignor[%%j]! .\%program_name%\
 )
 
-rem 安装依赖 && build
+rem 进入项目目录
 cd %program_name%
-yarn install >> %err_log% 2>&1
-pause
-yarn run build >> %err_log% 2>&1
-pause
 
-rem 把template安装到dist
-rem mkdir .\dist\template
-rem copy %cur_dist%template\. .\dist\template\
+rem 安装依赖 && build
+call yarn.cmd install 2> %err_log%
+call npm.cmd run build 2> %err_log%
+
+rem 创建template目录
+mkdir .\dist\template
+
+rem 把本地template下的文件copy到资源的dist下面
+copy %cur_dist%template\. .\dist\template\
+
+rem 运行winscp脚本
 winscp /script=%scp_script%
-echo `deploys completion at %time%` >> %suc_log%
+
+echo `deployed completion at %time%` >> %suc_log%
 @echo end
